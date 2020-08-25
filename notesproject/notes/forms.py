aa3fe4b2c08fc0818f7e579424d3e409c865ef41
kldavis52 <kyle.linnard.davis@gmail.com>
2020-08-25 14:38:34 -0400

    error loading list_notes.html

---
+++ notesproject/notes/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Notes
+
+
+class NotesForm(forms.ModelForm):
+
+    class Meta:
+        model = Notes
+        fields = [
+            'title',
+            'body',
+        ]
+
+
+)
--- notesproject/notes/views.py
+++ notesproject/notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
+
+
+def list_notes(request):
+    notes = Notes.objects.all()
+    return render(request, 'templates/list_notes.html', {'notes': notes})
+
+
+def notes_detail(request):
+    pass

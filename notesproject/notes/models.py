30a9a70fe53975ebf9b621281762dd1548635887
kldavis52 <kyle.linnard.davis@gmail.com>
2020-08-26 08:19:27 -0400

    added auto date_time field to Note model

--- notesproject/notes/models.py
+++ notesproject/notes/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
     body = models.TextField(max_length=10000, null=False, blank=False)
+    date_time = models.DateTimeField(
+        auto_now_add=True, null=False, blank=False)
 
     def __str__(self):
-        return f'{self.title} + \n\n + {self.body}'
+        return f'{self.title} {self.body}'

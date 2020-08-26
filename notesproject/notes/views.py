bc1ff41035fa2e5322e377b285a58d720a9bd396
kldavis52 <kyle.linnard.davis@gmail.com>
2020-08-26 05:01:19 -0400

    corrected notes_detail view and list_notes.html so that notes_detail.html was reachable

--- notesproject/notes/views.py
+++ notesproject/notes/views.py
@@ -9,6 +9,6 @@ def list_notes(request):
     return render(request, 'list_notes.html', {'notes': notes})
 
 
-def notes_detail(request):
-    note = Note.object.get(pk=pk)
-    return render(request, 'notes_detail.html',{'note': note})
+def notes_detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'notes_detail.html', {'note': note})
